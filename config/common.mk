7f160f3f9cb6ef59a654966df3a2f32b341a3674
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:17:47 -0700

    cm: Commonize cmsdk specific targets. Edited for VanillaUnicorn Change-Id: I5e2f0b84ed34244b7549daf91053258193dd716f
    Conflicts:
    	config/common.mk
    	config/themes_common.mk

--- config/common.mk
+++ config/common.mk
@@ -81,9 +81,8 @@ PRODUCT_COPY_FILES += \
 # T-Mobile theme engine
 include vendor/vu/config/themes_common.mk
 
-# Live lockscreen
-PRODUCT_COPY_FILES += \
-    vendor/vu/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
+# CMSDK
+include vendor/vu/config/cmsdk_common.mk
 
 # Required VanillaUnicorn packages
 PRODUCT_PACKAGES += \
@@ -117,17 +116,6 @@ PRODUCT_PACKAGES += \
 # Custom VU packages
 PRODUCT_PACKAGES += \
     VUPapers
-
-# CM Platform Library
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.platform-res \
-    org.cyanogenmod.platform \
-    org.cyanogenmod.platform.xml
-
-# CM Hardware Abstraction Framework
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.hardware \
-    org.cyanogenmod.hardware.xml
 
 # Extra tools in VU
 PRODUCT_PACKAGES += \
@@ -222,31 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.vu.display.version=$(VU_DISPLAY_VERSION)
 
 -include vendor/vu-priv/keys/keys.mk
-
-ifndef CM_PLATFORM_SDK_VERSION
-  # This is the canonical definition of the SDK version, which defines
-  # the set of APIs and functionality available in the platform.  It
-  # is a single integer that increases monotonically as updates to
-  # the SDK are released.  It should only be incremented when the APIs for
-  # the new release are frozen (so that developers don't write apps against
-  # intermediate builds).
-  CM_PLATFORM_SDK_VERSION := 5
-endif
-
-ifndef CM_PLATFORM_REV
-  # For internal SDK revisions that are hotfixed/patched
-  # Reset after each CM_PLATFORM_SDK_VERSION release
-  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
-  CM_PLATFORM_REV := 0
-endif
-
-# CyanogenMod Platform SDK Version used in VU
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
-
-# CyanogenMod Platform Internal used in VU
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.rev=$(CM_PLATFORM_REV)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
--- config/themes_common.mk
+++ config/themes_common.mk
@@ -4,5 +4,3 @@ PRODUCT_PACKAGES += \
     ThemeChooser \
     ThemesProvider
 
-PRODUCT_COPY_FILES += \
-    vendor/vu/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
